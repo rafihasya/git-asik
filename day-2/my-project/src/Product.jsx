8d8687bf97d95b9e24aecd752c00b05e53d6cfe2
agnesjuliana <agafordev@gmail.com>
2024-10-19 04:03:45 +0700

    feat : routing w3

---
+++ day-2/my-project/src/Product.jsx
@@ -0,0 +1,78 @@
+export default function Product() {
+  return (
+    <section className="flex flex-col w-fit mx-auto p-8">
+      <div className="flex flex-row justify-between items-center py-4">
+        <p className="text-xl">Wishlist (4)</p>
+        <button className="border w-fit border-neutral-900 py-2 px-4 rounded-md">
+          Move All To Bag
+        </button>
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col">
+            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
+              <img src="./assets/tas-gucci.png" alt="Tas Gucci" />
+            </figure>
+            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
+              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
+            </button>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold">Gucci duffle bag</p>
+            <p className="text-rose-600">$960</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col">
+            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
+              <img src="./assets/cpu-cooler.png" alt="CPU Cooler" />
+            </figure>
+            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
+              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
+            </button>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold">RGB liquid CPU Cooler</p>
+            <p className="text-rose-600">$1960</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col">
+            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
+              <img src="./assets/stick.png" alt="Gamepad" />
+            </figure>
+            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
+              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
+            </button>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold">GP11 Shooter USB Gamepad</p>
+            <p className="text-rose-600">$550</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col">
+            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
+              <img src="./assets/jacket.png" alt="Satin Jacket" />
+            </figure>
+            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
+              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
+            </button>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold">Quilted Satin Jacket</p>
+            <p className="text-rose-600">$750</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- day-2/my-project/src/main.jsx
+++ day-2/my-project/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Product from "./Product.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello</h1>
+  },
+  {
+    path: "/login",
+    element: <App/>,
+  },
+  {
+    path: "/product",
+    element: <Product/>,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
