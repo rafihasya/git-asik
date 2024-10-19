3fc6f770df0c6afef2d9f43d008b9986c79195e7
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-10-19 09:43:49 +0700

    feat: props and components

--- day-2/my-project/src/Product.jsx
+++ day-2/my-project/src/Product.jsx
@@ -9,76 +9,10 @@ export default function Product() {
       </div>
 
       <div className="flex flex-col lg:flex-row gap-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        {/* <div className="flex flex-col gap-2.5">
-          <div className="flex flex-col">
-            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
-              <img src="./assets/tas-gucci.png" alt="Tas Gucci" />
-            </figure>
-            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
-              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
-            </button>
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-semibold">Gucci duffle bag</p>
-            <p className="text-rose-600">$960</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2.5">
-          <div className="flex flex-col">
-            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
-              <img src="./assets/cpu-cooler.png" alt="CPU Cooler" />
-            </figure>
-            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
-              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
-            </button>
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-semibold">RGB liquid CPU Cooler</p>
-            <p className="text-rose-600">$1960</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2.5">
-          <div className="flex flex-col">
-            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
-              <img src="./assets/stick.png" alt="Gamepad" />
-            </figure>
-            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
-              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
-            </button>
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-semibold">GP11 Shooter USB Gamepad</p>
-            <p className="text-rose-600">$550</p>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2.5">
-          <div className="flex flex-col">
-            <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
-              <img src="./assets/jacket.png" alt="Satin Jacket" />
-            </figure>
-            <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
-              <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
-            </button>
-          </div>
-
-          <div className="flex flex-col">
-            <p className="font-semibold">Quilted Satin Jacket</p>
-            <p className="text-rose-600">$750</p>
-          </div>
-        </div> */}
+        <Card price="180" img="./assets/tas-gucci.png" title="Gucci Duffle Bag" />
+        <Card price="4000" img="./assets/cpu-cooler.png" title="Cooler" />
+        <Card price="2000" img="./assets/stick.png" title="Gamepad" />
+        <Card price="20" img="./assets/jacket.png" title="Jacket" />
       </div>
     </section>
   );
--- day-2/my-project/src/components/Card.jsx
+++ day-2/my-project/src/components/Card.jsx
@@ -1,9 +1,10 @@
-export default function Card() {
+/* eslint-disable react/prop-types */
+export default function Card({ img = './assets/tas-gucci.png', title = 'ini produk', price = '100' }) {
   return (
     <div className="flex flex-col gap-2.5">
       <div className="flex flex-col">
         <figure className="bg-neutral-200 flex justify-center items-center w-[270px] h-[250px] rounded-t-lg">
-          <img src="./assets/tas-gucci.png" alt="Tas Gucci" />
+          <img src={img} alt={img} />
         </figure>
         <button className="gap-2 bg-neutral-900 py-4 text-white flex items-center justify-center rounded-b-lg">
           <img src="./assets/cart.png" alt="Keranjang" /> Add To Cart
@@ -11,8 +12,8 @@ export default function Card() {
       </div>
 
       <div className="flex flex-col">
-        <p className="font-semibold">Gucci duffle bag</p>
-        <p className="text-rose-600">$960</p>
+        <p className="font-semibold">{title}</p>
+        <p className="text-rose-600">${price}</p>
       </div>
     </div>
   );
